6e0d1012b03248599fc076ea9f076ca84c250d2e
Namitha <namitharc84@gmail.com>
2018-04-17 14:03:33 +0530

    changed file code

--- 5.rb
+++ 5.rb
@@ -1,3 +1,13 @@
-File.open("testing.txt","a") do |line|
-	line.puts "\r" "Hello"
+# File.open("testing.txt","a") do |line|
+# 	line.puts "\r" "Hello"
+# end
+
+a = File.open("testing.txt","r") do |a|
+  text = a.read
+  puts text
 end
+b = File.open("testing.txt", 'a+')
+puts "Update the existing file"
+content = gets.to_s
+hi = b.puts(content)
+puts hi
