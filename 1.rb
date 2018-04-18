ac971fb986aace80433115898a65e5895bfc34f9
Namitha <namitharc84@gmail.com>
2018-04-18 11:42:42 +0530

    changed time to user inputs

--- 1.rb
+++ 1.rb
@@ -1,5 +1,27 @@
-a=["john", "jane", "sam", "rockwell", "edward", "brad", "jared"]
- h={}
-arr=[]
- b= h.merge!(QT001: "john",QT002:"jane",QT003:"sam",QT004:"rockwell",QT005:"edward",QT006:"brad",QT007:"jared")
- p b
+# a=["john", "jane", "sam", "rockwell", "edward", "brad", "jared"]
+#  h={}
+# arr=[]
+#  b= h.merge!(QT001: "john",QT002:"jane",QT003:"sam",QT004:"rockwell",QT005:"edward",QT006:"brad",QT007:"jared")
+#  p b
+
+
+# @people = [{"name"=>"NameA", "id"=>"001"}, {"name"=>"NameB", "id"=>"002"}]
+# select_tag "people", options_for_select(@people.collect {|p| [ p['name'], p['id'] ] })
+
+
+# a = ["John","Jane","Sam","Rockwell","Edward","Brad","Jared"]
+# b = {}
+# array = []
+# c = b.merge!(QT001:"John",QT002:"Jane",QT003:"Sam",QT004:"Rockwell",QT005:"Edward",QT006:"Brad",QT007:"Jared")
+# array.push(c)
+# p array
+
+
+
+a = ["John","Jane","Sam","Rockwell","Edward","Brad","Jared"]
+b = []
+a.each_with_index{|n,i| b << {i+1 => n}}
+p b
+
+
+
--- 3.rb
+++ 3.rb
@@ -1,5 +1,12 @@
+# require 'time'
+# a = Time.strptime("11pm", "%I%P").strftime("%H:%M")
+# p a
+# puts "Enter the time"
+# a = Time.now
+# p a.strftime('%I:%M %P')
+
 require 'time'
-a = Time.strptime("11pm", "%I%P").strftime("%H:%M")
-p a
-
-
+puts "Enter the time"
+a = gets.chomp
+p Time.parse(":#{a}")
+.strftime("%H:%M:%S")
